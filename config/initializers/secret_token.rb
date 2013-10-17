0ad0b5cecdc1423d22f3ac7978765aa5dc80fa4c
Radek <radek@rkarkut.pl>
2013-10-17 08:38:11 +0200

    clear rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkstore::Application.config.secret_token = '4d4824ff664e07052626489b5b180075021d5bbc11353ad737e0f02778b16ec87fafe99de51cf5362c995dbeb7457e2f87157cda6c45432553cbdb9cccc3c7bd'
